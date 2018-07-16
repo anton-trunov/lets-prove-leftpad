80aa0941b9465dd729ba244101119c601298fc70
Anton Trunov <anton.a.trunov@gmail.com>
2018-07-16 19:44:28 +0200

    Simplify proof of length_max_spec

--- coq_ssreflect/leftpad.v
+++ coq_ssreflect/leftpad.v
@@ -1,6 +1,5 @@
-(* TODO: move from seq to n.-tuple *)
-
-From mathcomp Require Import ssreflect ssrbool ssrnat eqtype seq.
+Set Warnings "-notation-overridden".
+From mathcomp Require Import ssreflect ssrbool ssrnat seq.
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
@@ -16,10 +15,7 @@ Definition leftpad (c : T) (n : nat) (s : seq T) : seq T :=
 
 Lemma length_max_spec c n s :
   size (leftpad c n s) = maxn n (size s).
-Proof.
-rewrite size_ncons maxnE.
-by case/orP: (leq_total n (size s)) => le; rewrite (eqP le) ?subnKC ?subnK.
-Qed.
+Proof. by rewrite size_ncons addnC maxnC maxnE. Qed.
 
 Lemma prefix_spec c n s :
   forall i, i < n - size s -> nth (leftpad c n s) i = c.
@@ -27,8 +23,6 @@ Proof. by move=> i; rewrite nth_ncons => ->. Qed.
 
 Lemma suffix_spec c n s :
   forall i, i < size s -> nth (leftpad c n s) (n - size s + i) = nth s i.
-Proof.
-by move=> i _; rewrite nth_ncons addKn ifN // -ltnNge leq_addr.
-Qed.
+Proof. by move=> i _; rewrite nth_ncons addKn ifN // -ltnNge leq_addr. Qed.
 
 End LeftPad.
